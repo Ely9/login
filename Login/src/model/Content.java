2306ed27da097efa91844d08e3d7e08dc953008c
Nyann <45115319+Ely9@users.noreply.github.com>
2020-01-06 22:16:31 +0900

    Add files via upload

---
+++ Login/src/model/Content.java
@@ -0,0 +1,43 @@
+package model;
+
+import java.io.Serializable;
+
+public class Content implements Serializable{
+
+
+
+	private String content,person;
+	private int limit_year,limit_month,limit_day;
+
+
+
+	public Content(String content,int limit_year,int limit_month,int limit_day,String person) {
+		this.content=content;
+		this.person=person;
+		this.limit_year = limit_year;
+		this.limit_month=limit_month;
+		this.limit_day=limit_day;
+	}
+
+
+	public String getContent() {
+		return content;
+	}
+
+	public String getPerson() {
+		return person;
+	}
+
+	public int getLimitYear() {
+		return limit_year;
+	}
+
+	public int getLimitMonth() {
+		return limit_month;
+	}
+
+	public int getLimitDatt() {
+		return limit_day;
+	}
+
+}
---
+++ Login/src/test/maindaotest.java
@@ -0,0 +1,18 @@
+package test;
+
+import dao.maindao;
+import model.Content;
+
+public class maindaotest {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+
+		maindao dao = new maindao();
+		Content contents = new Content("遊びに行く",2020,9,22,"なな");
+
+
+	}
+
+}
